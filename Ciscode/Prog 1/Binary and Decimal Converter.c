48b5499d41eb673a3e121d9537f914094cd4da42
Andrea To Chip <andreatochip@yahoo.com>
2025-09-28 11:09:14 +0800

    Create Binary and Decimal Converter.c

---
+++ Ciscode/Prog 1/Binary and Decimal Converter.c
@@ -0,0 +1,12 @@
+/*
+Nolvin uses all types of numbers in engineering, especially binary and decimal numbers. To make his studies easier, he wants to build a converter for the two number systems. This converter should:
+1. Ask the user to input a number.
+2. Ask the user to input the type of number system the number belongs to.
+3. Using a switch statement, call the corresponding calculations/functions for the chosen number system to convert it to the other system.
+4. Output the conversion result.
+
+HINTS:
+- For the binary to decimal conversion, utilize the modulo and division operators to break down the number by digit, then use the left shift (<<) operator to handle exponents.
+- For the decimal to binary conversion, utilize the modulo and division operators and convert the results into an array, then print the result out by array element.
+*/
+
