df5af313f52ed31f0911dd744645fb6bebcbae02
Andrea To Chip <andreatochip@yahoo.com>
2025-10-03 22:38:30 +0800

    Ciscode Version of To-Do List

--- Ciscode/Prog 2/To-Do List.c
+++ Ciscode/Prog 2/To-Do List.c
@@ -11,80 +11,88 @@ The program should:
 */
 
 #include <stdio.h>
+#include <string.h>
 
-int addTasks() {
-    printf("Enter a new task:");
-    scanf("%s", &tasks[length]);
-    length++;
+void addTasks(char tasks[][100], int *length) {
+    if (*length >= 10) {
+        printf("Task list is full!\n");
+        return;
+    }
+    printf("Enter a new task: \n");
+    getchar();  
+    fgets(tasks[*length], 100, stdin);
+    (*length)++;                 
 }
 
-int viewTasks() {
+void viewTasks(char tasks[][100], int *length) {
     int i;
 
-    printf("To-Do List:\n");
-        
-        if (length == 0) {
-            printf("No tasks left to do!");
-            return 1;
-        } else {
-            for (i = 0; i < length; i++) {
-                if (i == length - 1) {
-                    printf("\n%d. %s", i+1, tasks[i]);
-                } else {
-                    printf("\n%d. %s, ", i+1, tasks[i]);
-                }
-            }
-        }
-}
-
-int deleteTasks() {
-    int i, index, length;
-
-    if (length == 0) {
-            printf("No tasks available to delete!");
-            return 1;
-        } else {
-            for (i = 0; i < length; i++) {
-                if (i == length - 1) {
-                    printf("\n%d. %s", i+1, tasks[i]);
-                } else {
-                    printf("\n%d. %s, ", i+1, tasks[i]);
-                }
-            }
-        }
-
-    printf("\nEnter the number of the task you want to delete:");
-    scanf("%d", &index);
-
-    for (i = 0; i < length - index - 1; i++) {
-        tasks[index-1] = tasks[index];
+    printf("\n----- To-Do List -----\n");
+    if (*length == 0) {
+        printf("No tasks left to do!\n");
+        return;
     }
 
-    length--;
+    for (i = 0; i < *length; i++) {
+        printf("%d. %s\n", i + 1, tasks[i]);
+    }
+}
+
+void deleteTasks(char tasks[][100], int *length) {
+    int i, index;
+
+    if (*length == 0) {
+        printf("No tasks available to delete!\n");
+        return;
+    }
+
+    viewTasks(tasks, length);
+
+    printf("\nEnter the number of the task you want to delete: ");
+    scanf("%d", &index);
+
+    if (index < 1 || index > *length) {
+        printf("Invalid task number!\n");
+        return;
+    }
+
+    for (i = index - 1; i < *length - 1; i++) {
+        strcpy(tasks[i], tasks[i + 1]);
+    }
+
+    (*length)--;   
+    printf("Task %d deleted successfully!\n", index);
 }
 
 int main() {
-    int tasks[10], choice, i, length = 0;
+    int choice, length = 0;
+    char tasks[10][100];
 
-    printf("----- TO-DO LIST ----- \n");
-    printf("1. Add Task\n");
-    printf("2. View Tasks\n");
-    printf("3. Delete Task\n");
-    printf("Enter a choice:");
-    scanf("%d", &choice);
+    while (1) {
+        printf("\n----- TO-DO LIST MENU -----\n");
+        printf("1. Add Task\n");
+        printf("2. View Tasks\n");
+        printf("3. Delete Task\n");
+        printf("4. Exit\n");
+        printf("Enter a choice: ");
+        scanf(" %d", &choice);
 
-    switch (choice) {
-        case 1:
-            addTasks();
-            return 1;
-        case 2:
-            viewTasks();
-            return 1;
-        case 3:
-            deleteTasks();
-            return 1;
-        default:
-            break;
+        switch (choice) {
+            case 1:
+                addTasks(tasks, &length);
+                break;
+            case 2:
+                viewTasks(tasks, &length);
+                break;
+            case 3:
+                deleteTasks(tasks, &length);
+                break;
+            case 4:
+                printf("Closing the program.\n");
+                return 0;
+            default:
+                printf("Invalid choice, try again!\n");
+        }
     }
 
     return 0;
