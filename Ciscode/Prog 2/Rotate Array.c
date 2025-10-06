7fea4748e94e6fe5678277c449586e95b423d506
Andrea To Chip <andreatochip@yahoo.com>
2025-10-06 22:27:22 +0800

    Rotate Array description

--- Ciscode/Prog 2/Rotate Array.c
+++ Ciscode/Prog 2/Rotate Array.c
@@ -1,10 +1,12 @@
-Create a program that allows a user to input elements in an array of their chosen size, sort the elements in ascending order, and print out the new array.
+Create a program that allows a user to input elements in an array of their chosen size, shift the elements to the right by a certain index, and print out the new array.
 
 The program should:
 1. Ask the user to input the number of elements/size of the array.
-2. Ask the user to input all the elements of the array based on its size.
-3. Use a nested for loop to sort through the array and arrange all its elements in ascending order.
-4. Print out the new array in a proper bracketed format.
+2. Ask the user to input all the elements of the array.
+3. Ask the user to input an index by which the elements will shift.
+4. Shift all the elements of the array to the left by a certain index (left shift by 1 index at a time).
+(Elements that have reached the left end of the array (index 0) should be moved to the right end of the array and shifted accordingly.)
+5. Print out the new array in a proper bracketed format.
 */
 
 #include <stdio.h>
