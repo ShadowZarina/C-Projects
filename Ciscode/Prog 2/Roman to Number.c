f3103fcacd79146011e6ab249c8846948c1884ec
Andrea To Chip <andreatochip@yahoo.com>
2025-10-10 17:17:31 +0800

    Update Roman to Number.c

--- Ciscode/Prog 2/Roman to Number.c
+++ Ciscode/Prog 2/Roman to Number.c
@@ -1,1 +1,39 @@
+Create a C program that allows a user to input elements for 2 arrays, merge the two arrays together, and print out the new array in descending order.
+
+The program should:
+1. Ask the user to input the number of elements/size of the 2 arrays.
+2. Ask the user to input all the elements of the first and second arrays.
+3. Merge the elements of the 2 arrays into a single array.
+4. Using nested for loops, sort the elements of the new array in DESCENDING order.
+5. Print out the new array in a proper bracketed format.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int temp, i, x, y, length;
+    
+    // Ask for array sizes for array1 and array2
+    printf("Enter the number of elements for Array 1: ");
+    scanf("%d", &length1);
+
+    switch (c) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0; // Invalid Roman numeral character
+    }
+
+    for (i = 0; i < len; i++) {
+        int current_value = get_roman_value(s[i]);
+        int next_value = (i + 1 < len) ? get_roman_value(s[i+1]) : 0;
+
+        if (current_value < next_value) {
+    
+    return 0;
+}
 
