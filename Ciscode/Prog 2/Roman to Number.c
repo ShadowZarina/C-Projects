522dde60ceeb372c8680cfa40e80cec1d4e36296
Andrea To Chip <andreatochip@yahoo.com>
2025-10-11 14:14:46 +0800

    Update Roman to Number.c

--- Ciscode/Prog 2/Roman to Number.c
+++ Ciscode/Prog 2/Roman to Number.c
@@ -3,19 +3,16 @@ Create a C program that allows a user to input a Roman numeral, convert it into 
 The program should:
 1. Ask the user to input a string of Roman numerals.
 2. Convert the Roman numerals into an integer.
-2a.
-2b.
+2a. With a for loop, loop through each character and call getRomanValue(roman[i]);
+2b. The function should contain a switch case that assigns numerical values to each Roman numeral and return it to the main function.
 3. Print out the converted number.
 */
 
+// Header file declaration
 #include <stdio.h>
 
-int main() {
-    int temp, i, x, y, length;
-    
-    // Ask for array sizes for array1 and array2
-    printf("Enter the number of elements for Array 1: ");
-    scanf("%d", &length1);
+// Conversion function definition
+int getRomanValue(char roman[i]) {
 
     switch (c) {
         case 'I': return 1;
@@ -25,15 +22,39 @@ int main() {
         case 'C': return 100;
         case 'D': return 500;
         case 'M': return 1000;
-        default: return 0; // Invalid Roman numeral character
+        default: return 0; 
+    }
+}
+
+// Main function 
+int main() {
+    int i, length, result = 0;
+    
+    // Ask for Roman numeral input
+    printf("Enter a Roman numeral: ");
+    scanf("%s", roman);
+
+    length = strlen(roman);
+
+    // Loop through each character and convert it to numbers
+    for (i = 0; i < length; i++) {
+        int currentValue = getRomanValue(roman[i]);
+        int nextValue = i + 1 
+        if (i + 1 < length) {
+            getRomanValue(roman[i+1]);
+        } else {
+            0;
+        }
+
+        if (currentValue < nextValue) {
+            result += currentValue;
+        else if (currentValue > nextValue) {
+            result -= currentValue;
+        }
     }
 
-    for (i = 0; i < len; i++) {
-        int current_value = get_roman_value(s[i]);
-        int next_value = (i + 1 < len) ? get_roman_value(s[i+1]) : 0;
-
-        if (current_value < next_value) {
-    
+    printf("The Roman numeral converted equals to %d.", result);
+            
     return 0;
 }
 
