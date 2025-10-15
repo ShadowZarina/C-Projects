208af9109637a4581aa72b706c47fe23e4ee2935
Andrea To Chip <andreatochip@yahoo.com>
2025-10-15 17:13:08 +0800

    Update Find the Height of a Binary Tree.c

--- Ciscode/DSA/Find the Height of a Binary Tree.c
+++ Ciscode/DSA/Find the Height of a Binary Tree.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h> 
 
+typedef struct Node {
+    int data;        
+    struct Node *left; 
+    struct Node *right; 
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        // Handle memory allocation error
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+Node* insertNode(Node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+    if (data < root->data) {
+        root->left = insertNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = insertNode(root->right, data);
+    }
+    return root;
+}
